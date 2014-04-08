341bb3365e3e184fb5c3e997fe874bc78366489e
Nelson Filipe Silva <nelsonflsilva@gmail.com>
2014-04-08 21:25:53 +0100

    Added missing IServer Interface

---
+++ Shared/IServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public interface IServer
+    {
+        PadiInt CreatePadiInt(int txNumber, int uid);
+        PadiInt AccessPadiInt(int txNumber, int uid);
+        bool TryWrite(int txNumber, PadiInt padiInt);
+        int TxBegin();
+        bool TryTxCommit(int txNumber);
+        bool CanCommit(int txNumber);
+        bool TxCommit(int txNumber);
+        bool TxAbort(int txNumber);
+
+        bool Status();
+        bool Fail();
+        bool Freeze();
+        bool Recover();
+    }
+}
